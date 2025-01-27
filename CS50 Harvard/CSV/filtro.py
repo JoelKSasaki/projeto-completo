7472d49a2a01a033ccf2f8ebc4332a56c227e7c8
Joel Kagohara Sasaki <126785821+JoelKSasaki@users.noreply.github.com>
2025-01-27 10:16:27 -0300

    Add files via upload

---
+++ CS50 Harvard/CSV/filtro.py
@@ -0,0 +1,14 @@
+import csv
+
+with open("times.csv", "r") as file:
+    reader = csv.DictReader(file)
+    contador = {}
+    for row in reader:
+        favorite = row["time"]
+        if favorite in contador:
+            contador[favorite] += 1
+        else:
+            contador[favorite] = 1
+        
+for favorite in sorted(contador, key=contador.get, reverse=True):
+    print(f'{favorite}: {contador[favorite]}')
